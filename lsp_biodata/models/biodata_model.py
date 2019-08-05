629d713c0a7c918f70bf6c6f28a4a0ee9e68e174
abas <akhmadbasir5@gmail.com>
2019-08-06 02:34:52 +0700

    fixing wrong class name

--- lsp_biodata/models/biodata_model.py
+++ lsp_biodata/models/biodata_model.py
@@ -12,4 +12,4 @@ class Biodata(models.Model):
   # is_done = fields.Boolean('Done?')
   # active = fields.Boolean(string='Active?', default=True)
   user_id = fields.Many2one('res.users', string='Responsible', default=lambda self: self.env.user)
-  teams_ids = fields.Many2many('res.bio', string='Tim Bio')
+  teams_ids = fields.Many2many('res.biodata', string='Tim Bio')
--- lsp_biodata/models/res_biodata_model.py
+++ lsp_biodata/models/res_biodata_model.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields
 
-class ResPartner(models.Model):
-  _inherit = 'res.bio'
+class ResBiodata(models.Model):
+  _inherit = 'res.biodata'
 
   todo_ids = fields.Many2many('biodata.s', string='Bidata Creator')
