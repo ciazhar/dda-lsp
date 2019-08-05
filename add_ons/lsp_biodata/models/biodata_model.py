e2036f083994da986e3c92ea83ef0e076263a925
abas <akhmadbasir5@gmail.com>
2019-08-06 03:31:12 +0700

    update model biodata : add contrain unique nim nik email

--- add_ons/lsp_biodata/models/biodata_model.py
+++ add_ons/lsp_biodata/models/biodata_model.py
@@ -13,3 +13,15 @@ class Biodata(models.Model):
   # active = fields.Boolean(string='Active?', default=True)
   user_id = fields.Many2one('res.users', string='Responsible', default=lambda self: self.env.user)
   # teams_ids = fields.Many2many('res.biodata', string='Tim Bio')
+
+  _sql_constraints = [
+    ('name_unique',
+      'UNIQUE(nim)',
+      "The NIM must be unique"),
+    ('name_unique',
+      'UNIQUE(nik)',
+      "The NIK must be unique"),
+    ('name_unique',
+      'UNIQUE(email)',
+      "The EMAIL must be unique"),
+  ]
